ab3118a3b2a4aab3ef0940ed8b6073ada3d215f8
Nodara <nodo.gogaladze2002@gmail.com>
2025-08-31 16:07:19 +0000

    Redesign of routers

--- lectures/urls.py
+++ lectures/urls.py
@@ -1,24 +1,6 @@
 from django.urls import path, include
-from rest_framework_nested import routers
-from common.router.routers import router
-from .views import LectureViewSet, HomeworkAssignmentViewSet
-
 
 app_name = "lectures"
 
-lectures_router = routers.NestedSimpleRouter(router, r"courses", lookup="course")
-lectures_router.register(r"lectures", LectureViewSet, basename="course-lectures")
-
-
-assignments_router = routers.NestedSimpleRouter(
-    lectures_router, r"lectures", lookup="lecture"
-)
-assignments_router.register(
-    r"assignments", HomeworkAssignmentViewSet, basename="lecture-assignments"
-)
-
-
 urlpatterns = [
-    path("", include(lectures_router.urls)),
-    path("", include(assignments_router.urls)),
 ]
